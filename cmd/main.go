0002c7975890e51e89ec7a703ae12fe073b9b498
Cesar-Marcano <marcanocesar86@gmail.com>
2023-10-20 16:23:06 -0400

    started an http server on another thread

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"fmt"
-	discordBot "github.com/sleeyax/aternos-discord-bot"
-	"github.com/sleeyax/aternos-discord-bot/database"
 	"log"
+	"net/http"
 	"net/url"
 	"os"
 	"os/signal"
 	"syscall"
+
+	discordBot "github.com/sleeyax/aternos-discord-bot"
+	"github.com/sleeyax/aternos-discord-bot/database"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	// Inicia el servidor web en una goroutine
+	go startWebServer(port)
 	// Read configuration settings from environment variables
 	token := os.Getenv("DISCORD_TOKEN")
 	session := os.Getenv("ATERNOS_SESSION")
@@ -52,4 +61,15 @@ func main() {
 	interruptSignal := make(chan os.Signal, 1)
 	signal.Notify(interruptSignal, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-interruptSignal
+}
+
+func startWebServer(port string) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "¡Hola desde el servidor web en segundo plano!")
+	})
+
+	fmt.Printf("Iniciando el servidor web en segundo plano en el puerto %s...\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		fmt.Println("Error al iniciar el servidor web:", err)
+	}
 }
